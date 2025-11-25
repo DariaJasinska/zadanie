8abb6187b31968fc57f01c846e3d2b0679912dd9
DariaJasinska <dar327994@mat.umk.pl>
2025-11-26 00:08:22 +0100

    Zmodyfikowano liczba.cpp

--- liczba.cpp
+++ liczba.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+bool czy_bezkwadratowa( int n )
+{
+	if( n < 1 ) return false;
+
+	for( int i = 2; i <= sqrt(n); i++ )
+	{
+		if( n % ( i * i ) == 0 )
+		{
+			return false;
+		}
+	}
+	return true;
+}
 
 int main()
 {
 	int x;
 	cin >> x;
-	cout << x;
+
+	bool wynik = czy_kwadratowa(x);
+	cout << x << ", ";
+
+	if( wynik == true )
+	{
+		cout << "liczba bezkwadratowa";
+	}
+	else
+	{
+		cout << "liczba nie jest bezkwadratowa";
+	}
 }
